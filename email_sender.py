8c298108725b64092f681753ecae4682271b3746
Piotr Muskalski <piotr.muskalski@gmail.com>
2020-06-21 16:25:12 +0200

    Add an exception handling in email sending

--- email_sender.py
+++ email_sender.py
@@ -1,6 +1,8 @@
+import logging
 import smtplib
-
 from email.message import EmailMessage
+
+logging = logging.getLogger(__name__)
 
 
 def send_email(content):
@@ -12,8 +14,10 @@ def send_email(content):
     msg.set_content(content)
 
     # Send the message via our own SMTP server.
-    s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    s.login('***REMOVED***', '***REMOVED***')
-    s.send_message(msg)
-    s.quit()
-
+    try:
+        s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        s.login('***REMOVED***', '***REMOVED***')
+        s.send_message(msg)
+        s.quit()
+    except:
+        logging.exception('Problem with sending an e-mail')
