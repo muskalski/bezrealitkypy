068e13bba21f65025639a5e42e00a5d276bd9b50
Piotr Muskalski <piotr.muskalski@gmail.com>
2020-06-21 22:59:25 +0200

    Add retries for sending email

--- email_sender.py
+++ email_sender.py
@@ -1,5 +1,6 @@
 import logging
 import smtplib
+import time
 from email.message import EmailMessage
 
 logging = logging.getLogger(__name__)
@@ -14,10 +15,14 @@ def send_email(content):
     msg.set_content(content)
 
     # Send the message via our own SMTP server.
-    try:
-        s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        s.login('***REMOVED***', '***REMOVED***')
-        s.send_message(msg)
-        s.quit()
-    except:
-        logging.exception('Problem with sending an e-mail')
+    for _ in range(5):
+        try:
+            s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            s.login('***REMOVED***', '***REMOVED***')
+            s.send_message(msg)
+            s.quit()
+            return True
+        except:
+            logging.exception('Problem with sending an e-mail')
+        time.sleep(10)
+    return False
